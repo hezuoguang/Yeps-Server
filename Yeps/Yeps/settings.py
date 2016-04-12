4a6393c41785e9f21462746370a5dabec950ad0d
weimi <he_zuoguang@163.com>
2016-04-12 23:30:30 +0800

    uwsgi

--- Yeps/Yeps/settings.py
+++ Yeps/Yeps/settings.py
@@ -84,7 +84,7 @@ USE_L10N = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
--- Yeps/Yeps/urls.py
+++ Yeps/Yeps/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -17,4 +20,4 @@ urlpatterns += patterns('Yep.api_router',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^yeps/api/', "api_router"),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
