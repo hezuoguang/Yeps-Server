8e624d59805bb2d68fb9776f81afda0a9aa77ca5
weimi <he_zuoguang@163.com>
2016-05-03 16:52:29 +0800

    fix bugs

--- Yeps/Yeps/settings.py
+++ Yeps/Yeps/settings.py
@@ -85,7 +85,8 @@ USE_L10N = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-STATIC_ROOT = '/static/'
+# STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
